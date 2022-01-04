500c0c2c6d9352562bffbf9fa16085535e7bf8bf
gzwz <568602044@qq.com>
2022-01-04 16:18:14 +0800

    整合 Sleuth + zipkin

--- cloud-service/springcloud-provider/src/main/java/com/wlz/procontroller/ProviderController.java
+++ cloud-service/springcloud-provider/src/main/java/com/wlz/procontroller/ProviderController.java
@@ -1,15 +1,18 @@
 package com.wlz.procontroller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ProviderController {
 
     @RequestMapping("/provider/test/{name}")
     private String providertest(@PathVariable String name){
+        log.info("进入服务 springcloud-provider");
         return "服务提供方返回："+name;
     }
 }
